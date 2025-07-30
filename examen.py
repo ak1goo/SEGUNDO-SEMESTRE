0b2980a15341575d2805779e37339e61147cd210
Heidelle <legrandheidelle@gmail.com>
2025-07-29 22:54:17 -0600

    EXAMEN

--- examen.py
+++ examen.py
@@ -1,5 +1,6 @@
+#PARTE I
 #1 Lista
-#nombres = ["Ana", "Luis", "Pedro"]
+nombres = ["Ana", "Luis", "Pedro"]
 
 #2 Para eliminar un elemento de una lista en Python, 
 # se pueden utilizar varios métodos, entre ellos remove(), pop() y la palabra clave del. 
@@ -9,3 +10,44 @@
 while True:
     print("Hola")
     break
+#Imprime el texto "Hola"
+
+#4 
+diccionario=  {"Nombre:" "Heidelle Legrand", 
+               "Edad: " "18", 
+               "Carrera:" 
+               "Ingenieria en Sistemas"
+               }
+
+#5
+#Una tupla es inmutable, no se puede cambiar, mientras la lista si.
+
+#PARTE II
+#1 
+suma = 0
+
+while True:
+    numeros = int(input("Ingrese un número: "))
+    
+    if numeros == 0:
+        print(f"La suma es {suma}")
+        break
+        
+    suma += numeros
+
+#2
+numeros = [10,20,30,40]
+for numero in numeros:
+    print(numero * 2)
+
+#3
+productos = [
+    {"nombre": "Manzana", "precio": 3.5, "stock": 50},
+    {"nombre": "Pan", "precio": 2.0, "stock": 100},
+    {"nombre": "Jugo", "precio": 5.25, "stock": 30}
+]
+
+for producto in productos:
+    print(f"El producto {producto['nombre']} cuesta Q{producto['precio']} y hay {producto['stock']} unidades disponibles.")
+
+
