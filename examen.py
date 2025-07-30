372522a7c1e733f8a9fe7a5607f22d0861163066
Heidelle <legrandheidelle@gmail.com>
2025-07-29 22:43:03 -0600

    EXAMEN FINALIZADO

---
+++ examen.py
@@ -0,0 +1,11 @@
+#1 Lista
+#nombres = ["Ana", "Luis", "Pedro"]
+
+#2 Para eliminar un elemento de una lista en Python, 
+# se pueden utilizar varios métodos, entre ellos remove(), pop() y la palabra clave del. 
+# Cada uno tiene su propia forma de funcionar y se adapta a diferentes necesidades. 
+
+#3 
+while True:
+    print("Hola")
+    break
